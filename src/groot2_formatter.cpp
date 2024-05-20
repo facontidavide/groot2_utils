60ab24533dd17b4bc545c2f072c7e5a97f2913cb
Davide Faconti <davide.faconti@gmail.com>
2024-05-20 19:14:58 +0200

    more options

--- src/groot2_formatter.cpp
+++ src/groot2_formatter.cpp
@@ -7,24 +7,43 @@ int main(int argc, char** argv)
 {
   cxxopts::Options options("groot2_formatter", "Parse an XML file and create a new one "
                                                "using the Groot2 rules");
-
-  options.add_options()("i,input", "Input file", cxxopts::value<std::string>())(
-      "o,output", "Output file", cxxopts::value<std::string>())
-      // ("m,models", "(Optional) BT models file", cxxopts::value<std::string>())
-      ("h,help", "Print this message");
+  // clang-format off
+  options.add_options()
+    ("i,input", "Input file", cxxopts::value<std::string>())
+    ("o,output", "Output file", cxxopts::value<std::string>())
+    ("check", "Check if the XML is correctly formatted")
+    ("inplace", "Change the file inplace")
+    ("h,help", "Print this message");
+  // clang-format on
 
   auto result = options.parse(argc, argv);
   if(result.count("help"))
   {
     std::cout << options.help() << std::endl;
-    exit(0);
+    return 0;
   }
 
-  std::string file_in = result["input"].as<std::string>();
+  const std::string file_in = result["input"].as<std::string>();
   std::cout << "Input file: " << file_in << std::endl;
 
+  const bool check = result.count("check");
+  const bool inplace = result.count("inplace");
+  const bool has_fileout = result.count("output");
+
+  if(!inplace && !check && !has_fileout)
+  {
+    std::cout << "Use either the argument --output, --check or --inplace " << std::endl;
+    return -1;
+  }
+  const auto file_out = has_fileout ? result["output"].as<std::string>() : std::string();
+
+  std::ifstream stream_in(file_in);
+  std::stringstream buffer;
+  buffer << stream_in.rdbuf();
+  const std::string xml_in = buffer.str();
+
   tinyxml2::XMLDocument doc;
-  auto parsed = doc.LoadFile(file_in.c_str());
+  auto parsed = doc.Parse(xml_in.data(), xml_in.size());
 
   if(parsed != tinyxml2::XML_SUCCESS)
   {
@@ -35,18 +54,36 @@ int main(int argc, char** argv)
   groot2::CustomXmlPrinter printer;
   doc.Print(&printer);
 
-  std::string out(printer.CStr(), printer.CStrSize() - 1);
+  const std::string xml_out(printer.CStr(), printer.CStrSize() - 1);
 
-  if(result.count("output"))
+  const bool is_same = (xml_in == xml_out);
+
+  if(is_same)
   {
-    std::string file_out = result["output"].as<std::string>();
-    std::cout << "Output file: " << file_out << std::endl;
-    std::ofstream ofstream(file_out, std::ofstream::out);
-    ofstream << out;
+    std::cout << "Input and output XMLs are the SAME\n";
   }
   else
   {
-    std::cout << "\n Output:\n" << out << std::endl;
+    std::cout << "Input and output XMLs are the NOT the same\n";
+  }
+
+  if(!is_same && (inplace || (has_fileout && file_in == file_out)))
+  {
+    std::ofstream ofstream(file_in, std::ofstream::out);
+    std::cout << "Writing new XML into file: " << file_in << std::endl;
+    ofstream << xml_out;
+  }
+
+  if(has_fileout && file_in != file_out)
+  {
+    std::ofstream ofstream(file_out, std::ofstream::out);
+    std::cout << "Writing new XML into file: " << file_out << std::endl;
+    ofstream << xml_out;
+  }
+
+  if(check && !is_same)
+  {
+    return 1;
   }
 
   return 0;
